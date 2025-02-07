4b50dc17442b711c8f42195b736c665e4d345583
Lisa Sakai Quinley <quinl162@newschool.edu>
2025-02-07 12:03:30 -0500

    playing with vue and practicing how to create a project

---
+++ my-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloLisa msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloLisa from './components/HelloLisa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloLisa
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
