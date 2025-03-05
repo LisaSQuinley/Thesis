f43a45a6205bcc0f6f5a59de6baed22c3385b0fd
Lisa Sakai Quinley <quinl162@newschool.edu>
2025-03-04 21:06:29 -0500

    finally getting something to work with Vue, like leaflet

---
+++ lsq-thesis/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HelloMorocco msg="A Hot Topic for Morocco"/>
+    <MapView />  <!-- Updated name here -->
+  </div>
+</template>
+
+<script>
+import HelloMorocco from './components/HelloMorocco.vue'
+import MapView from './components/MapView.vue'  // Updated import here
+
+export default {
+  name: 'App',
+  components: {
+    HelloMorocco, 
+    MapView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lsq-thesis/src/components/HelloMorocco.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Extreme Weather Events and Climate Change</h2>
+    <p>
+      To check out other projects Lisa has created, visit
+      <a href="https://lsq.design" target="_blank" rel="noopener">lsq.design</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloMorocco',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
